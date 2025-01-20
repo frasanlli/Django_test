6a79285f66d9824651b98f522afd45ddfb308440
frasanlli <146319006+frasanlli@users.noreply.github.com>
2025-01-20 19:34:29 +0100

    class 12th. Discover admin panel

--- online_shop/manage_orders/admin.py
+++ online_shop/manage_orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from manage_orders.models import Costumers, Items, Orders
+
 # Register your models here.
+
+#To modify which fields are appear on the admin tab of the class, <object table>, we need to create a class with same propertie's name.
+#The properties specified will appear, the other ones not
+#then the class must be added to the function admin.site.register(<Class to filter>, <Class filter>)
+class Admin_costumer(admin.ModelAdmin):
+    list_display = ("name","address", "phone")
+    search_fields = ("name", "phone")
+
+admin.site.register(Costumers, Admin_costumer)
+admin.site.register(Items)
+admin.site.register(Orders)
--- online_shop/manage_orders/models.py
+++ online_shop/manage_orders/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 # Create your models here.
-
+#to add new features and use them, first we need to repeat migrations
+#python manage.py makemigrations
+#python manage.py migrate
 
 class Costumers(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
-    email = models.EmailField()
+    #To enable blank or null fields set properties (blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
     phone = models.CharField(max_length=9)
 
 
@@ -14,6 +17,9 @@ class Items(models.Model):
     name = models.CharField(max_length=30)
     category = models.CharField(max_length=30)
     price = models.IntegerField()
+
+    def __str__(self):
+        return f"Name: {self.name}. Category: {self.category}. Price: {self.price}"
 
 
 class Orders(models.Model):
