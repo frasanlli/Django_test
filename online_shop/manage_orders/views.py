466cfc223d8980fe6dec13ae6a831f45dc979115
frasanlli <146319006+frasanlli@users.noreply.github.com>
2025-01-23 12:21:24 +0100

    class 15th. break

--- online_shop/manage_orders/views.py
+++ online_shop/manage_orders/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from manage_orders.models import Items
 
 # Create your views here.
 
@@ -10,13 +11,21 @@ def search_item(request):
 def search(request):
 
     message: str = ""
+    search_item: str = request.GET["itm"]
 
     if request.GET["itm"]:
-        message = f"Searched item: '{request.GET["itm"]}'"
+        if len(request.GET["itm"]) <= 20:
+            #This is a LIKE sql query
+            message = f"Searched item: '{request.GET["itm"]}'"
+            filter_item = Items.objects.filter(name__icontains=search_item)
 
-        if len(request.GET["itm"]) > 20:
+            return render(request, "search_result.html", {"items": filter_item, "query": search_item})
+
+        else:
             message = "The searched item is too long"
+
+            return HttpResponse(message)
     else:
         message = "Empty search"
 
-    return HttpResponse(message)
+        return HttpResponse(message)
